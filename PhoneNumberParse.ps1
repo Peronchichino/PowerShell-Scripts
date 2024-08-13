2520c530ecbebc91f9fb2cc89f28437596673416
Lukas Buchmayer <lukasbuchmayer@gmail.com>
2024-08-13 17:08:54 +0200

    Update PhoneNumberParse.ps1

--- PhoneNumberParse.ps1
+++ PhoneNumberParse.ps1
@@ -9,6 +9,8 @@
     The parsed number of the user is then assigned as the new number of the user in AD
 #>
 
+#todo: e5 + active filter, parameter for exec (object), 
+
 function WriteLog{    
     Param ([string]$logString)
     $dateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
@@ -18,10 +20,8 @@ function WriteLog{
 
 $csvLogging = "x"
 
-
 $benchmark = [System.Diagnostics.Stopwatch]::StartNew()
 
-#TODO: refactor
 $groupId = "x"
 
 $bla = @("Buchmayer Lukas")
@@ -41,23 +41,28 @@ foreach($member in $members){
     $memberId = $memberDets.UserPrincipalName
 
     try{
-
+        $checkE5 = Get-AzureADUserMembership -ObjectID $member.ObjectId | where-object objectid -eq "xx"
         #azure attributes for phone numbers: BusinessPhone -> TelephoneNumber; MobilePhone -> Mobile
         if($member -ne $null){
             $num = $($member.TelephoneNumber) -replace '[()\s-]', ''
-            Write-Host($num)
-            $domainbuff = $member.Mail -split '@'
-            if($domainbuff.Count -eq 2){
-                $domain = $domainbuff[1]
+            if($checkE5 -ne $null){
+                Set-AzureADUser -ObjectId $member.ObjectId -TelephoneNumber $num
+
+                Set-CsPhoneNumberAssignment -Identity $memberId -PhoneNumber $num -PhoneNumberType DirectRouting
+                Get-CsOnlineUser $memberId | Grant-CsOnlineVoiceRoutingPolicy -PolicyName "CAL_VRP_INT"
+                Get-CsOnlineUser $memberId | Grant-CsTenantDialPlan -PolicyName "CAL DP 79070"
+                Get-CsOnlineUser $memberId | Set-CsTeamsCallingPolicy -Policyname "General - no forwarding to external numbers"
+                
+                $msg = "[INFO] Number parsed: $($memberId) (E5) -> $($num)"
+                WriteLog($msg)
+                Write-Host($msg)
             }
 
-            #change number
-            Set-AzureADUser -ObjectId $member.ObjectId -TelephoneNumber $num
-            $msg = "[INFO] Number parsed: $($memberId) -> $($num)"
-            WriteLog($msg)
         }
 
-        Write-Host($memberId+": "+$num+" | "+$domain);
+        $msg = "[RES] $($memberId): $($num), | $($domain)"
+        WriteLog($msg)
+        Write-Host($msg)
     } catch {
         throw $_.Exception
     }
